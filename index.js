6069ddaea29070675b9399d89bff8608d8074dc7
grez72 <grez72@gmail.com>
2020-07-05 15:36:29 -0400

    grr, how to add dependencies

--- index.js
+++ index.js
@@ -1,72 +1,73 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const fs = require('fs');
-const AWS = require('aws-sdk');
+// const core = require('@actions/core');
+// const github = require('@actions/github');
+// const fs = require('fs');
+// const AWS = require('aws-sdk');
 
 async function run() {
-    try {
+    console.log("Hello?");
+    // try {
 
-        const functionName = core.getInput('function-name');
-        const package = core.getInput('package');
-        const AWS_SECRET_ACCESS_KEY = core.getInput('AWS_SECRET_ACCESS_KEY');
-        const AWS_ACCESS_KEY_ID = core.getInput('AWS_ACCESS_KEY_ID');
-        const AWS_REGION = core.getInput('AWS_REGION');
-        const aliasName = core.getInput('alias-name');
-        const publish = core.getInput('publish');
-        const dryRun = core.getInput('dry-run');
+    //     const functionName = core.getInput('function-name');
+    //     const package = core.getInput('package');
+    //     const AWS_SECRET_ACCESS_KEY = core.getInput('AWS_SECRET_ACCESS_KEY');
+    //     const AWS_ACCESS_KEY_ID = core.getInput('AWS_ACCESS_KEY_ID');
+    //     const AWS_REGION = core.getInput('AWS_REGION');
+    //     const aliasName = core.getInput('alias-name');
+    //     const publish = core.getInput('publish');
+    //     const dryRun = core.getInput('dry-run');
 
-        console.log(`Updating Function Name ${functionName} with ${package}!`);
+    //     console.log(`Updating Function Name ${functionName} with ${package}!`);
       
-        var zipBuffer = fs.readFileSync(`./${package}`);
-        core.debug('Package put into memory buffer');
+    //     var zipBuffer = fs.readFileSync(`./${package}`);
+    //     core.debug('Package put into memory buffer');
 
-        const lambda = new AWS.Lambda({
-            apiVersion: '2015-03-31',
-            region: AWS_REGION,
-            secretAccessKey: AWS_SECRET_ACCESS_KEY,
-            accessKeyId: AWS_ACCESS_KEY_ID,
-            maxRetries: 3,
-            sslEnabled: true,
-            logger: console,
-        });
+    //     const lambda = new AWS.Lambda({
+    //         apiVersion: '2015-03-31',
+    //         region: AWS_REGION,
+    //         secretAccessKey: AWS_SECRET_ACCESS_KEY,
+    //         accessKeyId: AWS_ACCESS_KEY_ID,
+    //         maxRetries: 3,
+    //         sslEnabled: true,
+    //         logger: console,
+    //     });
 
-        const update_params = {
-            FunctionName: functionName,
-            Publish: publish, // publish new version
-            ZipFile: zipBuffer,
-            DryRun: dryRun
-        };
+    //     const update_params = {
+    //         FunctionName: functionName,
+    //         Publish: publish, // publish new version
+    //         ZipFile: zipBuffer,
+    //         DryRun: dryRun
+    //     };
 
-        const data = await lambda.updateFunctionCode(update_params).promise();
+    //     const data = await lambda.updateFunctionCode(update_params).promise();
 
-        // avoid printing out environment variables:
-        const data_log = (({ FunctionName, FunctionArn, Version, RevisionId, LastUpdateStatus }) => ({ FunctionName, FunctionArn, Version, RevisionId, LastUpdateStatus }))(data);
-        console.log("\nsuccess! updated lambda function:", data_log, "\n");
+    //     // avoid printing out environment variables:
+    //     const data_log = (({ FunctionName, FunctionArn, Version, RevisionId, LastUpdateStatus }) => ({ FunctionName, FunctionArn, Version, RevisionId, LastUpdateStatus }))(data);
+    //     console.log("\nsuccess! updated lambda function:", data_log, "\n");
 
-        console.log(`Updating Alias Name ${aliasName} to ${functionName}:${data.Version}!`);
+    //     console.log(`Updating Alias Name ${aliasName} to ${functionName}:${data.Version}!`);
 
-        const alias_params = {
-            FunctionName: functionName, /* required */
-            Name: aliasName, /* required */
-            FunctionVersion: data.Version,
-            // RevisionId: 'STRING_VALUE',
-            //RoutingConfig: {
-            //  AdditionalVersionWeights: {
-            //    '<AdditionalVersion>': 'NUMBER_VALUE',
-            //  }
-            //}
-        };
+    //     const alias_params = {
+    //         FunctionName: functionName, /* required */
+    //         Name: aliasName, /* required */
+    //         FunctionVersion: data.Version,
+    //         // RevisionId: 'STRING_VALUE',
+    //         //RoutingConfig: {
+    //         //  AdditionalVersionWeights: {
+    //         //    '<AdditionalVersion>': 'NUMBER_VALUE',
+    //         //  }
+    //         //}
+    //     };
 
-        if (dryRun !== true) {
-            const alias = await lambda.updateAlias(alias_params).promise();
-            const alias_log = (({ AliasArn, Name, FunctionVersion, Description, RevisionId }) => ({ AliasArn, Name, FunctionVersion, Description, RevisionId }))(data);
-            console.log("\nsuccess! updated alias:");
-            console.log(alias_log);
-        }        
+    //     if (dryRun !== true) {
+    //         const alias = await lambda.updateAlias(alias_params).promise();
+    //         const alias_log = (({ AliasArn, Name, FunctionVersion, Description, RevisionId }) => ({ AliasArn, Name, FunctionVersion, Description, RevisionId }))(data);
+    //         console.log("\nsuccess! updated alias:");
+    //         console.log(alias_log);
+    //     }        
 
-    } catch (error) {
-        core.setFailed(error.message);
-    }
+    // } catch (error) {
+    //     core.setFailed(error.message);
+    // }
 }
   
 run();
